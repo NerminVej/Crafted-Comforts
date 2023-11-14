f9be177882aca450432dd6998d635e52641b2a75
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-14 18:04:07 +0100

    cleaned up some boilerplate react code and added the hero background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import heroImage from "../assets/hero-image.svg";
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div
+      className="h-screen relative flex flex-col items-center"
+      style={{
+        background: `url(${heroImage})`,
+        backgroundPosition: "bottom",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
+};
