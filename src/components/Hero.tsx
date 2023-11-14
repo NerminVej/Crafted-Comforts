f3549c1c349b5d9ef1fdcb79cc174631aae6b0d9
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-14 18:17:55 +0100

    implemented the icon to the project

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,6 +17,7 @@ export const Hero = () => {
       }}
     >
       <div className="relative w-full max-w-[1490px] flex items-center justify-between pt-10 mx-auto px-10"></div>
+      <img src={logo} />
       <ul className="hidden md:flex items-center gap-10 lg:grap-[60px]">
         <NavLink to="services">Service </NavLink>
         <NavLink to="products">Shop </NavLink>
