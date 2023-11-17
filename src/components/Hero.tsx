1836328a3771a25a054bfab3538a84a7c919d7a1
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-17 19:48:56 +0100

    created the FadeIn componeent

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,50 +25,52 @@ export const Hero = () => {
       <div
         className="relative w-full max-w-[1490px] flex 
         items-center justify-between pt-10 mx-auto px-10"
-      ></div>
-      <img src={logo} />
-      <ul className="hidden md:flex items-center gap-10 lg:gap-[68px]">
-        {" "}
-        <NavLink to="services">Service </NavLink>
-        <NavLink to="products">Shop </NavLink>
-        <NavLink to="reference">Reference </NavLink>
-        <NavLink to="care">Care </NavLink>
-      </ul>
-
-      <img src={cartIcon} className="hidden md:block cursor-pointer" />
-      <HiMenuAlt3
-        size={30}
-        className="block md:hidden cursor-pointer text-white"
-        onClick={() => setShowMobileMenu((prev) => !prev)}
-      />
-
-      <div
-        className={`block md:hidden w-full fixed ${
-          !showMobileMenu ? "-top-[410px]" : "top-0"
-        } left-0 bg-[#dde0e5] h-[410px] transition-all duration-[800ms] shadow-xl z-10 py-8 px-12 rounded-b-xl`}
       >
-        <AiOutlineClose
-          size={25}
-          className="absolute top-5 right-5 cursor-pointer"
-          onClick={() => setShowMobileMenu(false)}
+        <img src={logo} />
+        <ul className="hidden md:flex items-center gap-10 lg:gap-[68px]">
+          {" "}
+          <NavLink to="services">Service </NavLink>
+          <NavLink to="products">Shop </NavLink>
+          <NavLink to="reference">Reference </NavLink>
+          <NavLink to="care">Care </NavLink>
+        </ul>
+
+        <img src={cartIcon} className="hidden md:block cursor-pointer" />
+        <HiMenuAlt3
+          size={30}
+          className="block md:hidden cursor-pointer text-white"
+          onClick={() => setShowMobileMenu((prev) => !prev)}
         />
 
-        <ul className="pt-[60px] items-center flex flex-col gap-8">
-          <NavLink to="services" mobileMenu>
-            Services
-          </NavLink>
-          <NavLink to="products" mobileMenu>
-            Shop
-          </NavLink>
-          <NavLink to="reference" mobileMenu>
-            Reference
-          </NavLink>
-          <NavLink to="care" mobileMenu>
-            Care
-          </NavLink>
-        </ul>
-        <img src={cartIcon} className="mt-8 mx-auto cursor-pointer" alt="" />
+        <div
+          className={`block md:hidden w-full fixed ${
+            !showMobileMenu ? "-top-[410px]" : "top-0"
+          } left-0 bg-[#dde0e5] h-[410px] transition-all duration-[800ms] shadow-xl z-10 py-8 px-12 rounded-b-xl`}
+        >
+          <AiOutlineClose
+            size={25}
+            className="absolute top-5 right-5 cursor-pointer"
+            onClick={() => setShowMobileMenu(false)}
+          />
+
+          <ul className="pt-[60px] items-center flex flex-col gap-8">
+            <NavLink to="services" mobileMenu>
+              Services
+            </NavLink>
+            <NavLink to="products" mobileMenu>
+              Shop
+            </NavLink>
+            <NavLink to="reference" mobileMenu>
+              Reference
+            </NavLink>
+            <NavLink to="care" mobileMenu>
+              Care
+            </NavLink>
+          </ul>
+          <img src={cartIcon} className="mt-8 mx-auto cursor-pointer" alt="" />
+        </div>
       </div>
+      
     </div>
   );
 };
---
+++ src/components/ui/FadeIn.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FadeIn = () => {
+  return <div>
+    
+  </div>;
+};
+
+export default FadeIn;
