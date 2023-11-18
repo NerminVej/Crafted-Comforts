829e417e60b0492047ce613ec6f0ad364cb8e700
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-18 09:24:30 +0100

    more or less finished the hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,6 +9,7 @@ import { heroTitle, heroSubtitle } from "../utils/data.ts";
 
 import { HiMenuAlt3 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
+
 import FadeIn from "./ui/FadeIn.tsx";
 
 export const Hero = () => {
@@ -27,16 +28,15 @@ export const Hero = () => {
         className="relative w-full max-w-[1490px] flex 
         items-center justify-between pt-10 mx-auto px-10"
       >
-        <img src={logo} />
+        <img src={logo} alt="" />
         <ul className="hidden md:flex items-center gap-10 lg:gap-[68px]">
-          {" "}
-          <NavLink to="services">Service </NavLink>
-          <NavLink to="products">Shop </NavLink>
-          <NavLink to="reference">Reference </NavLink>
-          <NavLink to="care">Care </NavLink>
+          <NavLink to="services">Services</NavLink>
+          <NavLink to="products">Shop</NavLink>
+          <NavLink to="reference">Reference</NavLink>
+          <NavLink to="care">Care</NavLink>
         </ul>
 
-        <img src={cartIcon} className="hidden md:block cursor-pointer" />
+        <img src={cartIcon} className="hidden md:block cursor-pointer" alt="" />
         <HiMenuAlt3
           size={30}
           className="block md:hidden cursor-pointer text-white"
@@ -68,23 +68,21 @@ export const Hero = () => {
               Care
             </NavLink>
           </ul>
+
           <img src={cartIcon} className="mt-8 mx-auto cursor-pointer" alt="" />
         </div>
       </div>
-      
-      
+
       <FadeIn delay={0.2} direction="down" padding fullWidth>
         <h1 className="mt-[90px] text-center text-5xl leading-tight xs:text-[64px] text-white max-w-[1050px]">
           {heroTitle}
         </h1>
       </FadeIn>
-
       <FadeIn delay={0.4} direction="down" padding fullWidth>
         <h5 className="mt-6 text-center text-lg xs:text-xl text-white max-w-[500px]">
           {heroSubtitle}
         </h5>
       </FadeIn>
-
       <FadeIn delay={0.2} direction="up" padding fullWidth>
         <div className="relative w-full xs:w-[460px] mt-11">
           <input
@@ -101,6 +99,7 @@ export const Hero = () => {
         </div>
       </FadeIn>
 
+      <div className="absolute h-[50px] xs:h-[150px] bottom-0 w-full bg-[linear-gradient(180deg,_#ffffff00_0%,_#FFF_100%)]" />
     </div>
   );
 };
