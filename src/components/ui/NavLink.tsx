63426ac7458e598512cbb9e93495437893a50a29
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-18 09:28:42 +0100

    created the typescript props for both fadein and navlink components

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -1,6 +1,19 @@
+import { ReactNode } from "react";
 import { Link } from "react-scroll";
 
-const NavLink = ({ children, to, offset = -50, mobileMenu = false }) => {
+interface NavLinkProps {
+  children: ReactNode;
+  to: string;
+  offset?: number;
+  mobileMenu?: boolean;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({
+  children,
+  to,
+  offset = -50,
+  mobileMenu = false,
+}) => {
   const className = mobileMenu
     ? "text-lg text-black hover:text-white cursor-pointer hover:bg-[linear-gradient(90deg,_#2AF598_0%,_#009EFD_100%)] w-full text-center"
     : "text-lg lg:text-xl text-white cursor-pointer hover:bg-[linear-gradient(90deg,_#2AF598_0%,_#009EFD_100%)] hover:bg-clip-text hover:text-transparent";
