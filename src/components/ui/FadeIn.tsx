92b33ccb2af860171da3b371408b1769ba909c26
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-18 09:16:06 +0100

    created the motion div for the fadein effect

--- src/components/ui/FadeIn.tsx
+++ src/components/ui/FadeIn.tsx
@@ -1,5 +1,7 @@
+// framer motion
 import { motion, useAnimation, useInView } from "framer-motion";
 
+// react
 import { useEffect, useRef } from "react";
 
 const FadeIn = ({ children, delay, direction, fullWidth, padding }) => {
@@ -14,15 +16,40 @@ const FadeIn = ({ children, delay, direction, fullWidth, padding }) => {
     }
   }, [isInView, controls]);
 
-  return     <div
-  ref={ref}
-  className={`${fullWidth ? "w-full" : "w-auto"} ${
-    padding ? "px-10" : "px-0"
-  }  flex items-center justify-center`}
->
-  test
-
-  </div>;
+  return (
+    <div
+      ref={ref}
+      className={`${fullWidth ? "w-full" : "w-auto"} ${
+        padding ? "px-10" : "px-0"
+      }  flex items-center justify-center`}
+    >
+      <motion.div
+        variants={{
+          hidden: {
+            opacity: 0,
+            x: direction === "right" ? -100 : direction === "left" ? 100 : 0,
+            y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+          },
+          visible: {
+            opacity: 1,
+            x: 0,
+            y: 0,
+          },
+        }}
+        initial="hidden"
+        animate={controls}
+        transition={{
+          duration: 1.25,
+          type: "tween",
+          delay: delay,
+          ease: [0.25, 0.25, 0.25, 0.75],
+        }}
+        className="w-full flex items-center justify-center"
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
 };
 
 export default FadeIn;
