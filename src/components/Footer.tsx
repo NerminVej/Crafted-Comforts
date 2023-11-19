536e1a95e3dc865654db64fba882a114fb9f7e3a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-19 10:53:36 +0100

    started to create the overall design for the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,13 @@
+import {
+  footerLinksColumnOne,
+  footerLinksColumnTwo,
+  newsletter,
+} from "../utils/data";
 
 export const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return     <div className="bg-[linear-gradient(133deg,_#566270_0%,_#283444_100%)]">
+      <div className="max-w-[1490px] mx-auto px-10 py-[120px]">
+
+</div>
+  </div>;
+};
