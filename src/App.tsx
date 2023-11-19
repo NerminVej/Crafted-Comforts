401ffc211fcf1a9d300ce38c9e3e72c34beac3c5
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-19 10:47:20 +0100

    created the Care component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Hero } from "./components/Hero";
 import Services from "./components/Services";
 import Products from "./components/Products";
 import { Reference } from "./components/Reference";
+import Care from "./components/Care";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <Products />
       <Reference />
+      <Care />
     </div>
   );
 }
