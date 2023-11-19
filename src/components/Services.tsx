d892bd67555cd1c7e5a8c4a06e7633e79a55dd99
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-19 10:32:56 +0100

    created the div container for the services component

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,8 +1,16 @@
+// local imports
+import { services } from "../utils/data";
+import FadeIn from "./ui/FadeIn.tsx";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div
+      id="services"
+      className="mt-[160px] max-w-[1490px] mx-auto px-10 flex flex-col xs:flex-row gap-12 xs:gap-6 xs:justify-between w-full"
+    >
+     
+    </div>
+  );
+};
 
-export default Services
+export default Services;
