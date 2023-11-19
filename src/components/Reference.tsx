dfe776f993d9ebc744d443b2c35ef8b31723e489
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-19 10:40:56 +0100

    did all the imports for the Reference component

--- src/components/Reference.tsx
+++ src/components/Reference.tsx
@@ -1,8 +1,11 @@
+import { referenceTitle, referenceSubtitle } from "../utils/data";
+
+import imageOne from "../assets/image-one.svg";
+import imageTwo from "../assets/image-two.svg";
+import imageThree from "../assets/image-three.svg";
+
+import FadeIn from "./ui/FadeIn";
 
 export const Reference = () => {
-  return (
-    <div>
-        Test
-    </div>
-  )
-}
+  return <div>Test</div>;
+};
