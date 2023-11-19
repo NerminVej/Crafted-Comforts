7a6e78211df4d778e2e9a46ad17b5747425d191a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-19 10:48:35 +0100

    created the title styling for the title of the care component

--- src/components/Care.tsx
+++ src/components/Care.tsx
@@ -1,8 +1,18 @@
+import { careTitle, careSubtitle, careList } from "../utils/data";
+import imageFour from "../assets/image-four.svg";
+
+import FadeIn from "./ui/FadeIn";
 
 const Care = () => {
   return (
-    <div>Care</div>
-  )
-}
+    <div id="care" className="px-10 max-w-[1490px] mx-auto mb-[160px]">
+      <FadeIn delay={0.2} direction="down" fullWidth={false} padding={false}>
+        <h1 className="text-5xl lg:text-[64px] font-medium text-fontBlack mb-6 text-center">
+          {careTitle}
+        </h1>
+      </FadeIn>
+    </div>
+  );
+};
 
-export default Care
+export default Care;
