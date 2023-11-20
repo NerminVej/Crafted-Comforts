d1423cbcb4a67b514c8f9d1a5aee0a3b3ac2b615
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-11-20 18:28:44 +0100

    removed more imports

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,8 +1,6 @@
 import { productsTitle, productsBtnText, products } from "../utils/data";
 
 import arrowIcon from "../assets/arrow-icon.svg";
-import starsIcon from "../assets/stars-icon.svg";
-import plusIcon from "../assets/plus-icon.svg";
 
 import FadeIn from "./ui/FadeIn";
 import Product from "./Product";
